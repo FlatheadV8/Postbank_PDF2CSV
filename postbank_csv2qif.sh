ffc06050b8bd21dedaa6ca9ee3bdc4ad3186e214
Manfred <FlatheadV8@users.noreply.github.com>
2015-05-31 17:53:54 +0200

    Update postbank_csv2qif.sh
    seit der vorletzten Änderung an der Datei /home/bin/postbank_pdf2csv.sh konnte diese Datei nichts mehr mit der CSV-Datei anfangen, jetzt habe ich diese Datei wieder für die ausgegebene CSV-Datei angepasst

--- postbank_csv2qif.sh
+++ postbank_csv2qif.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+#==============================================================================#
 #
 # CSV -> QIF (Quicken Interchange Format)
 #
+#------------------------------------------------------------------------------#
 #
 # dieses Skript wandelt die mit "postbank_pdf2csv.sh" generieren CSV-Datei in eine QIF-Datei
 # für GnuCache um
@@ -10,6 +12,12 @@
 # Quelle (2014-12-07):
 # http://wiki.gnucash.org/wiki/FAQ#Q:_How_do_I_convert_from_CSV.2C_TSV.2C_XLS_.28Excel.29.2C_or_SXC_.28OpenOffice.org_Calc.29_to_a_QIF.3F
 #
+#==============================================================================#
+
+VERSION="v2015053100"
+
+#------------------------------------------------------------------------------#
+### Hinweis geben
 
 if [ -z "${1}" ] ; then
         echo "${0} Datei1.csv"
@@ -28,17 +36,16 @@ NEUERNAME="$(echo "${CSVDATEI}" | sed 's/[( )][( )]*/_/g' | rev | sed 's/.*[.]//
 
 (echo '!Type:Bank'
 	###
+	### CSV: Betrag;Buchung;Wert;Vorgang/Buchungsinformation;
+	###
 	### echo -n "Buchung;Wert;Vorgang/Buchungsinformation;Soll;Haben"
 	###
-	cat "${CSVDATEI}" | grep -Ev '^Buchung;' | while read ZEILE
+	cat "${CSVDATEI}" | grep -Ev '^Betrag;Buchung;Wert;Vorgang/Buchungsinformation;' | while read ZEILE
 	do
-		BUCHUNGSDATUM="$(echo "${ZEILE}" | awk -F';' -v jahr=${JAHR} '{ gsub("[.]"," "); print $1,jahr }' | awk '{print $3"-"$2"-"$1}')"
-		WERTSTELLUNGSDATUM="$(echo "${ZEILE}" | awk -F';' -v jahr=${JAHR} '{ gsub("[.]"," "); print $2,jahr }' | awk '{print $3"-"$2"-"$1}')"
-		VERWENDUNGSZWECK="$(echo "${ZEILE}" | awk -F';' '{ print $3 }')"
-		NEG_BETR="$(echo "${ZEILE}" | awk -F';' '{print $4}' | awk '{print $2}')"
-		POS_BETR="$(echo "${ZEILE}" | awk -F';' '{print $5}' | awk '{print $2}')"
-		BETRAG="$(if [ -n "${NEG_BETR}" ] ; then echo "-${NEG_BETR}"; elif [ -n "${POS_BETR}" ] ; then echo "+${POS_BETR}"; fi)"
-		echo -e "D${BUCHUNGSDATUM}\nM${WERTSTELLUNGSDATUM} ${VERWENDUNGSZWECK}\nT${BETRAG}\n^"
+		BETRAG="$(echo "${ZEILE}" | awk -F';' '{print $1}')"
+		BUCHUNGSDATUM="$(echo "${ZEILE}" | awk -F';' -v jahr=${JAHR} '{ gsub("[.]"," "); print $2,jahr }' | awk '{print $3"-"$2"-"$1}')"
+		VERWENDUNGSZWECK="$(echo "${ZEILE}" | sed 's/;/|/;s/;/|/;' | awk -F'|' '{ print $3 }')"
+		echo -e "D${BUCHUNGSDATUM}\nM${VERWENDUNGSZWECK}\nT${BETRAG}\n^"
 	done
 ) > ${NEUERNAME}.qif
 
